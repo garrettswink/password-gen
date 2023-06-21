53506cb539d88f0bccaa124911d06c24aca07ae6
Garrett <garrettswink421@gmail.com>
2023-06-21 18:08:45 -0400

    Final tweaks.

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+
+//Initial variables for the assigment
 var characterLength = 8;
 var choiceArray = [];
 
@@ -5,8 +7,6 @@ var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
-
 
 // Defines generateBtn variable, matching it to generate Button-ID in HTML
 var generateBtn = document.querySelector("#generate");
@@ -35,7 +35,7 @@ function generatePassword () {
     return password;
 }
 
-//Prompt 
+//Prompts & Password Customization 
 function getPrompts(){
     choiceArray = [];
 
